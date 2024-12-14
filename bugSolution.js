4a94795c7aaddf9885cfc859a828f6d7d003fcb5
BugHunter-X <askzenai@gmail.com>
2024-12-14 05:40:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.on('error', err => {
+  console.error('Server error:', err);
+});
+
+server.listen(8080);
